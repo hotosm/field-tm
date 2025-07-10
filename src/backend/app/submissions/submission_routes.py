3367d2ec85eecec19fd00d699f355ce6762b6743
sujanadh <sujanadh10@gmail.com>
2025-07-10 14:07:23 +0545

    fix(backend): create valid geojson from javarosa, update osm conflation with raw-data-api-py

--- src/backend/app/submissions/submission_routes.py
+++ src/backend/app/submissions/submission_routes.py
@@ -458,9 +458,8 @@ async def conflate_geojson(
         )
         osm_category = project.osm_category
         input_features = submission_geojson["features"]
-
-        # FIXME to be replaced with raw-data-api-py & osm_rawdata removed
-        osm_features = postgis_utils.get_osm_geometries(osm_category, task_geojson)
+        
+        osm_features = await postgis_utils.get_osm_geometries(osm_category, task_geojson)
         conflated_features = postgis_utils.conflate_features(
             input_features, osm_features.get("features", []), remove_conflated
         )
