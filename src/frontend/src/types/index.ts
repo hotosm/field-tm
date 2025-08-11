3529b203fcbb5d0e880b30475ed1fc974981ac5c
Nishit Suwal <suwalnishit@gmail.com>
2025-08-11 06:15:20 +0000

    feat(types): generic type for UseQueryOptions and UseMutationOptions

--- src/frontend/src/types/index.ts
+++ src/frontend/src/types/index.ts
@@ -1,3 +1,6 @@
+import type { AxiosResponse } from 'axios';
+import type { UseQueryOptions, QueryKey, UseMutationOptions } from '@tanstack/react-query';
+
 export * from '@/api/auth/types';
 export * from '@/api/central/types';
 export * from '@/api/helper/types';
@@ -6,3 +9,18 @@ export * from '@/api/project/types';
 export * from '@/api/submission/types';
 export * from '@/api/task/types';
 export * from '@/api/user/types';
+
+// Generic type for UseQueryOptions
+export type TQueryOptions<TData, TError = Error, TQueryKey extends QueryKey = QueryKey> = UseQueryOptions<
+  AxiosResponse<TData>,
+  TError,
+  TData,
+  TQueryKey
+>;
+
+// Generic type for UseMutationOptions
+export type TMutationOptions<TData, TVariables, TError = Error> = UseMutationOptions<
+  AxiosResponse<TData>,
+  TError,
+  TVariables
+>;
