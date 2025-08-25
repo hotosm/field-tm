316458eedf8d7b225abebac05d85668870100c9e
Anuj-Gupta4 <anujgupta.4388@gmail.com>
2025-08-25 13:42:30 +0545

    feat: only add translation for the set language in mandatory fields and skip for bare label

--- src/backend/packages/osm-fieldwork/osm_fieldwork/form_components/translations/__init__.py
+++ src/backend/packages/osm-fieldwork/osm_fieldwork/form_components/translations/__init__.py
@@ -2,6 +2,8 @@
 
 import json
 from pathlib import Path
+import re
+from typing import Optional
 
 # Load translation JSON files as dictionaries
 def _load_translations(file_name):
@@ -21,7 +23,7 @@ INCLUDED_LANGUAGES = {
 
 translations = {f"{key}({value})": _load_translations(f"{value}.json") for key, value in INCLUDED_LANGUAGES.items()}
 
-def add_label_translations(base: dict) -> dict:
+def add_label_translations(base: dict, label_cols: Optional[list[str]] = []) -> dict:
     field_name = base.get("name")
 
     # If name is a list (from a DataFrame row-style dict), unwrap it
@@ -34,10 +36,35 @@ def add_label_translations(base: dict) -> dict:
     if not isinstance(field_name, str):
         return base
 
-    for lang_key, lang_dict in translations.items():
-        label_key = f"label::{lang_key}"
+    if "label" in label_cols:
+        lang_dict = translations.get("english(en)", {})
         label_value = lang_dict.get(field_name)
         if label_value:
-            base[label_key] = label_value
+            base["label"] = label_value
+
+    elif label_cols:
+        for col in label_cols:
+            if not col.startswith("label::"):
+                continue
+
+            match = re.match(r"label::([^(]+)(?:\(([^)]+)\))?", col)
+            if not match:
+                continue
+
+            lang_key = match.group(1).strip().lower()
+            lang_code = match.group(2) or INCLUDED_LANGUAGES.get(lang_key)
+
+            trans_key = f"{lang_key}({lang_code})" if lang_code else lang_key
+            lang_dict = translations.get(trans_key, {})
+
+            label_value = lang_dict.get(field_name)
+            if label_value:
+                base[col] = label_value
+    else:
+        for lang_key, lang_dict in translations.items():
+            label_key = f"label::{lang_key}"
+            label_value = lang_dict.get(field_name)
+            if label_value:
+                base[label_key] = label_value
 
     return base
