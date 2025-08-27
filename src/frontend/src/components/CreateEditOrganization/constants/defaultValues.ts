726c8a1c566ad6b9e19528b5b22fda19c61d5ee1
Nishit Suwal <suwalnishit@gmail.com>
2025-08-27 12:04:56 +0000

    feat(defaultValues): createEdit organization default values add

---
+++ src/frontend/src/components/CreateEditOrganization/constants/defaultValues.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod/v4';
+import { createOrganizationValidationSchema } from '../validation/CreateEditOrganization';
+
+export const defaultValues: z.infer<typeof createOrganizationValidationSchema> = {
+  name: '',
+  url: '',
+  associated_email: '',
+  description: '',
+  odk_server_type: '',
+  odk_central_url: '',
+  odk_central_user: '',
+  odk_central_password: '',
+  community_type: '',
+  logo: null,
+  update_odk_credentials: false,
+};
