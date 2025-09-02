bb6a475c347164d70215619e02711d930ca62dd2
Nishit Suwal <81785002+NSUWAL123@users.noreply.github.com>
2025-09-02 13:27:52 +0545

    Feat/tanstack query integration (#2814)
    * refactor(organisationInfo): replace api calls with tanstack query
    
    * refactor(manageUsers): fetch userlist with useQuery
    
    * fix(isEmpty): fix params type
    
    * refactor(homeService): remove homeService
    
    * refactor(manageUsers): prettier, eslint fixes
    
    * feat(createProject): replace organization api call with tanstack query
    
    * feat(createProject): minimal api call using tanstack query
    
    * refactor(project): implement useQuery for project users api call
    
    * refactor(organisation): relpace api calls with tanstack query
    
    * refactor(organisation): refactor repititive code
    
    * refactor(organisation): replace mui components
    
    * refactor(manageAdmins): replace api calls with tanstack query
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/frontend/src/views/Organisation.tsx
+++ src/frontend/src/views/Organisation.tsx
@@ -1,228 +1,159 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import CoreModules from '@/shared/CoreModules';
 import AssetModules from '@/shared/AssetModules';
-import { MyOrganisationDataService, OrganisationDataService } from '@/api/OrganisationService';
 import { user_roles } from '@/types/enums';
 import { GetOrganisationDataModel } from '@/models/organisation/organisationModel';
 import OrganisationGridCard from '@/components/organisation/OrganisationGridCard';
-import windowDimention from '@/hooks/WindowDimension';
-import { useAppDispatch, useAppSelector } from '@/types/reduxTypes';
 import useDocumentTitle from '@/utilfunctions/useDocumentTitle';
 import { useHasManagedAnyOrganization, useIsAdmin } from '@/hooks/usePermissions';
 import OrganizationCardSkeleton from '@/components/Skeletons/Organization/OrganizationCardSkeleton';
+import {
+  useGetMyOrganisationsQuery,
+  useGetOrganisationsQuery,
+  useGetUnapprovedOrganisationsQuery,
+} from '@/api/organisation';
+import Button from '@/components/common/Button';
+import { useNavigate } from 'react-router-dom';
+import Searchbar from '@/components/common/SearchBar';
+import useDebouncedInput from '@/hooks/useDebouncedInput';
+
+type OrganisationSectionPropsType = {
+  isLoading: boolean;
+  orgList: any[] | null | undefined;
+  searchKeyword: string;
+};
+
+type TabType = {
+  label: string;
+  isActive: boolean;
+  onClick: () => void;
+};
+
+const filteredBySearch = (data: GetOrganisationDataModel[], searchKeyword: string) => {
+  const filteredCardData: GetOrganisationDataModel[] = data?.filter((d) =>
+    d.name.toLowerCase().includes(searchKeyword.toLowerCase()),
+  );
+  return filteredCardData;
+};
+
+const OrganisationListSection = ({ isLoading, orgList, searchKeyword }: OrganisationSectionPropsType) => {
+  if (isLoading) {
+    return (
+      <div className="fmtm-grid fmtm-grid-cols-1 md:fmtm-grid-cols-2 lg:fmtm-grid-cols-3 fmtm-gap-5 fmtm-w-full">
+        {Array.from({ length: 12 }).map((_, i) => (
+          <OrganizationCardSkeleton key={i} />
+        ))}
+      </div>
+    );
+  }
+  if (orgList) {
+    return (
+      <OrganisationGridCard filteredData={filteredBySearch(orgList, searchKeyword)} allDataLength={orgList.length} />
+    );
+  }
+  return null;
+};
+
+const Tab = ({ label, isActive, onClick }: TabType) => {
+  console.log(isActive);
+  return (
+    <div
+      onClick={onClick}
+      className={`fmtm-w-fit hover:fmtm-bg-[#999797] hover:fmtm-text-white fmtm-px-4 fmtm-py-1 fmtm-cursor-pointer fmtm-duration-200 fmtm-font-medium ${isActive ? ' fmtm-bg-[#808080] fmtm-text-white' : 'fmtm-bg-white fmtm-text-[#808080]'}`}
+    >
+      {label}
+    </div>
+  );
+};
 
 const Organisation = () => {
   useDocumentTitle('Organizations');
-  const dispatch = useAppDispatch();
-  const { type } = windowDimention();
+  const navigate = useNavigate();
   const isAdmin = useIsAdmin();
   const hasManagedAnyOrganization = useHasManagedAnyOrganization();
 
   const [searchKeyword, setSearchKeyword] = useState<string>('');
   const [activeTab, setActiveTab] = useState<0 | 1>(0);
   const [verifiedTab, setVerifiedTab] = useState<boolean>(true);
-  const [myOrgsLoaded, setMyOrgsLoaded] = useState(false);
   const authDetails = CoreModules.useAppSelector((state) => state.login.authDetails);
-  const defaultTheme = useAppSelector((state) => state.theme.hotTheme);
 
-  const organisationData = useAppSelector((state) => state.organisation.organisationData);
-  const myOrganisationData = useAppSelector((state) => state.organisation.myOrganisationData);
+  const [searchTextData, handleChangeData] = useDebouncedInput({
+    ms: 400,
+    init: '',
+    onChange: (e) => {
+      setSearchKeyword(e.target.value);
+    },
+  });
 
-  const organisationDataLoading = useAppSelector((state) => state.organisation.organisationDataLoading);
-  const myOrganisationDataLoading = useAppSelector((state) => state.organisation.myOrganisationDataLoading);
-  // loading states for the organisations from selector
+  const { data: organisationList, isLoading: isOrganisationListLoading } = useGetOrganisationsQuery({
+    options: { queryKey: ['get-organisation-list'], enabled: activeTab === 0 && verifiedTab },
+  });
 
-  let cardsPerRow = new Array(
-    type == 'xl' ? 3 : type == 'lg' ? 3 : type == 'md' ? 3 : type == 'sm' ? 2 : type == 's' ? 2 : 1,
-  ).fill(0);
-  // calculate number of cards to display according to the screen size
+  const { data: myOrganisationList, isLoading: isMyOrganisationListLoading } = useGetMyOrganisationsQuery({
+    options: { queryKey: ['get-my-organisation-list'], enabled: activeTab === 1 },
+  });
 
-  const handleSearchChange = (event) => {
-    setSearchKeyword(event.target.value);
-  };
-  const filteredBySearch = (data: GetOrganisationDataModel[], searchKeyword: string) => {
-    const filteredCardData: GetOrganisationDataModel[] = data?.filter((d) =>
-      d.name.toLowerCase().includes(searchKeyword.toLowerCase()),
-    );
-    return filteredCardData;
-  };
-
-  useEffect(() => {
-    if (verifiedTab) {
-      dispatch(OrganisationDataService(`${import.meta.env.VITE_API_URL}/organisation`));
-    } else {
-      dispatch(OrganisationDataService(`${import.meta.env.VITE_API_URL}/organisation/unapproved`));
-    }
-  }, [verifiedTab]);
-
-  const loadMyOrganisations = () => {
-    if (!myOrgsLoaded) {
-      dispatch(MyOrganisationDataService(`${import.meta.env.VITE_API_URL}/organisation/my-organisations`));
-      setMyOrgsLoaded(true);
-    }
-    setActiveTab(1);
-  };
+  const { data: unapprovedOrganisationList, isLoading: isUnapprovedOrganisationListLoading } =
+    useGetUnapprovedOrganisationsQuery({
+      options: { queryKey: ['get-unapproved-organisation-list'], enabled: activeTab === 0 && !verifiedTab },
+    });
 
   return (
-    <CoreModules.Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        background: '#f5f5f5',
-        flex: 1,
-        gap: 2,
-      }}
-    >
-      <div className="md:fmtm-hidden fmtm-border-b-white fmtm-border-b-[1px]">
-        <div className="fmtm-flex fmtm-justify-between fmtm-items-center">
-          <h1 className="fmtm-text-xl sm:fmtm-text-2xl fmtm-mb-1 sm:fmtm-mb-2">MANAGE ORGANIZATIONS</h1>
+    <div className="fmtm-flex fmtm-flex-col fmtm-gap-3">
+      {/* tab filters */}
+      <div className="fmtm-w-full fmtm-flex fmtm-items-center fmtm-justify-between fmtm-flex-wrap fmtm-gap-y-3">
+        <div className="fmtm-flex fmtm-gap-3">
+          <Tab label="ALL" isActive={activeTab === 0} onClick={() => setActiveTab(0)} />
+          <Tab label="MY ORGANIZATIONS" isActive={activeTab === 1} onClick={() => setActiveTab(1)} />
+          {(!hasManagedAnyOrganization || isAdmin) && (
+            <Button variant="secondary-red" onClick={() => navigate('/organization/new')} className="!fmtm-py-1">
+              <AssetModules.AddIcon />
+              NEW
+            </Button>
+          )}
         </div>
-      </div>
-      <div className="fmtm-flex fmtm-flex-col md:fmtm-flex-row md:fmtm-justify-between md:fmtm-items-center fmtm-gap-2">
-        <CoreModules.Box>
-          <CoreModules.Tabs sx={{ minHeight: 'fit-content' }}>
-            <CoreModules.Tab
-              label="All"
-              sx={{
-                background: activeTab === 0 ? 'grey' : 'white',
-                color: activeTab === 0 ? 'white' : 'grey',
-                minWidth: 'fit-content',
-                width: 'auto',
-                '&:hover': { backgroundColor: '#999797', color: 'white' },
-                fontSize: ['14px', '16px', '16px'],
-                minHeight: ['26px', '36px', '36px'],
-                height: ['30px', '36px', '36px'],
-                px: ['12px', '16px', '16px'],
-              }}
-              className="fmtm-duration-150"
-              onClick={() => setActiveTab(0)}
-            />
-            <CoreModules.Tab
-              label="My Organizations"
-              sx={{
-                background: activeTab === 1 ? 'grey' : 'white',
-                color: activeTab === 1 ? 'white' : 'grey',
-                marginLeft: ['8px', '12px', '12px'],
-                minWidth: 'fit-content',
-                width: 'auto',
-                '&:hover': { backgroundColor: '#999797', color: 'white' },
-                fontSize: ['14px', '16px', '16px'],
-                minHeight: ['26px', '36px', '36px'],
-                height: ['30px', '36px', '36px'],
-                px: ['12px', '16px', '16px'],
-              }}
-              className="fmtm-duration-150"
-              onClick={() => loadMyOrganisations()}
-            />
-            {(!hasManagedAnyOrganization || isAdmin) && (
-              <CoreModules.Link to={'/organization/new'}>
-                <CoreModules.Button
-                  variant="outlined"
-                  color="error"
-                  startIcon={<AssetModules.AddIcon />}
-                  sx={{
-                    marginLeft: ['8px', '12px', '12px'],
-                    minWidth: 'fit-content',
-                    width: 'auto',
-                    fontWeight: 'bold',
-                    minHeight: ['26px', '36px', '36px'],
-                    height: ['30px', '36px', '36px'],
-                    px: ['12px', '16px', '16px'],
-                  }}
-                >
-                  New
-                </CoreModules.Button>
-              </CoreModules.Link>
-            )}
-          </CoreModules.Tabs>
-        </CoreModules.Box>
         {authDetails && authDetails['role'] && authDetails['role'] === user_roles.ADMIN && activeTab === 0 && (
-          <CoreModules.Box>
-            <CoreModules.Tabs sx={{ minHeight: 'fit-content' }}>
-              <CoreModules.Tab
-                label="To be Verified"
-                sx={{
-                  background: !verifiedTab ? 'grey' : 'white',
-                  color: !verifiedTab ? 'white' : 'grey',
-                  minWidth: 'fit-content',
-                  width: 'auto',
-                  '&:hover': { backgroundColor: '#999797', color: 'white' },
-                  fontSize: ['14px', '16px', '16px'],
-                  minHeight: ['26px', '36px', '36px'],
-                  height: ['30px', '36px', '36px'],
-                  px: ['12px', '16px', '16px'],
-                }}
-                className="fmtm-duration-150"
-                onClick={() => setVerifiedTab(false)}
-              />
-              <CoreModules.Tab
-                label="Verified"
-                sx={{
-                  background: verifiedTab ? 'grey' : 'white',
-                  color: verifiedTab ? 'white' : 'grey',
-                  marginLeft: ['8px', '12px', '12px'],
-                  minWidth: 'fit-content',
-                  width: 'auto',
-                  '&:hover': { backgroundColor: '#999797', color: 'white' },
-                  fontSize: ['14px', '16px', '16px'],
-                  minHeight: ['26px', '36px', '36px'],
-                  height: ['30px', '36px', '36px'],
-                  px: ['12px', '16px', '16px'],
-                }}
-                className="fmtm-duration-150"
-                onClick={() => setVerifiedTab(true)}
-              />
-            </CoreModules.Tabs>
-          </CoreModules.Box>
+          <div className="fmtm-flex fmtm-gap-3">
+            <Tab label="TO BE VERIFIED" isActive={!verifiedTab} onClick={() => setVerifiedTab(false)} />
+            <Tab label="VERIFIED" isActive={verifiedTab} onClick={() => setVerifiedTab(true)} />
+          </div>
         )}
       </div>
-      <CoreModules.Box>
-        <CoreModules.TextField
-          id="search-organization"
-          variant="outlined"
-          size="small"
-          placeholder="Search organization"
-          value={searchKeyword}
-          onChange={handleSearchChange}
-          InputProps={{
-            startAdornment: (
-              <CoreModules.InputAdornment position="start">
-                <AssetModules.SearchIcon />
-              </CoreModules.InputAdornment>
-            ),
-          }}
-          className="fmtm-min-w-[14rem] lg:fmtm-w-[20%]"
+
+      <Searchbar
+        value={searchTextData}
+        onChange={handleChangeData}
+        wrapperStyle="fmtm-min-w-[14rem] fmtm-max-w-[20.6rem]"
+        isSmall
+      />
+
+      {/* Verified Organisations */}
+      {activeTab === 0 && verifiedTab && (
+        <OrganisationListSection
+          isLoading={isOrganisationListLoading}
+          orgList={organisationList}
+          searchKeyword={searchKeyword}
         />
-      </CoreModules.Box>
-      {activeTab === 0 ? (
-        organisationDataLoading ? (
-          <div className="fmtm-grid fmtm-grid-cols-1 md:fmtm-grid-cols-2 lg:fmtm-grid-cols-3 fmtm-gap-5 fmtm-w-full">
-            {Array.from({ length: 12 }).map((_, i) => (
-              <OrganizationCardSkeleton key={i} />
-            ))}
-          </div>
-        ) : (
-          <OrganisationGridCard
-            filteredData={filteredBySearch(organisationData, searchKeyword)}
-            allDataLength={organisationData?.length}
-          />
-        )
-      ) : null}
-      {activeTab === 1 ? (
-        organisationDataLoading ? (
-          <div className="fmtm-grid fmtm-grid-cols-1 md:fmtm-grid-cols-2 lg:fmtm-grid-cols-3 fmtm-gap-5 fmtm-w-full">
-            {Array.from({ length: 12 }).map((_, i) => (
-              <OrganizationCardSkeleton key={i} />
-            ))}
-          </div>
-        ) : (
-          <OrganisationGridCard
-            filteredData={filteredBySearch(myOrganisationData, searchKeyword)}
-            allDataLength={myOrganisationData?.length}
-          />
-        )
-      ) : null}
-    </CoreModules.Box>
+      )}
+
+      {/* Unverified Organisations */}
+      {activeTab === 0 && !verifiedTab && (
+        <OrganisationListSection
+          isLoading={isUnapprovedOrganisationListLoading}
+          orgList={unapprovedOrganisationList}
+          searchKeyword={searchKeyword}
+        />
+      )}
+
+      {/* My Organisations */}
+      {activeTab === 1 && (
+        <OrganisationListSection
+          isLoading={isMyOrganisationListLoading}
+          orgList={myOrganisationList}
+          searchKeyword={searchKeyword}
+        />
+      )}
+    </div>
   );
 };
 
