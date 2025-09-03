4326eb795be34475d1e9d786d42f47624f74541d
Nishit Suwal <suwalnishit@gmail.com>
2025-09-03 09:23:48 +0000

    fix(useCase): update style

--- src/frontend/src/components/Landing/UseCase.tsx
+++ src/frontend/src/components/Landing/UseCase.tsx
@@ -24,7 +24,7 @@ const useCaseList: useCaseType[] = [
 
 const UseCase = () => {
   return (
-    <div className="fmtm-flex fmtm-px-[3rem] md:fmtm-px-[4.5rem] fmtm-gap-10 fmtm-flex-col md:fmtm-flex-row">
+    <div className="fmtm-flex fmtm-px-[3rem] md:fmtm-px-[4.5rem] fmtm-gap-10 md:fmtm-flex-row fmtm-flex-col-reverse">
       <img
         src={MappingImg}
         className="fmtm-w-[14rem] md:fmtm-w-[18.75rem] lg:fmtm-w-[22rem] fmtm-h-fit fmtm-rounded-2xl fmtm-mx-auto"
