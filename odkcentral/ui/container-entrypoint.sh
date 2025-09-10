d9b698c89fda6cdf48a9f87f069940ab467a4a5b
spwoodcock <sam.woodcock@protonmail.com>
2025-09-10 18:20:10 +0100

    build: fix frontend file permissions for prod deploy

--- odkcentral/ui/container-entrypoint.sh
+++ odkcentral/ui/container-entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 # Copy frontend to attached volume
 echo "Syncing files from /app --> /frontend."
 rclone sync /app /frontend
+echo "Updating permissions --> 777."
 chmod 777 -R /frontend
 echo "Sync done."
 
