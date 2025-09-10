9f7a17ff7bcb20d10ed5f6ce7cae79f3f9c40f17
spwoodcock <sam.woodcock@protonmail.com>
2025-09-10 18:46:55 +0100

    build: fix setting 777 permissions modsec error, set nginx group instead

--- odkcentral/ui/container-entrypoint.sh
+++ odkcentral/ui/container-entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 # Copy frontend to attached volume
 echo "Syncing files from /app --> /frontend."
 rclone sync /app /frontend
-echo "Updating permissions --> 777."
-chmod 777 -R /frontend
+echo "Updating directory group --> nginx."
+chown -R nginx:nginx /frontend
 echo "Sync done."
 
 # Successful exit (stop container)
