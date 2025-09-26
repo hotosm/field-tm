1a47a21dc42aaddec8e673be71526d0679e4fb71
Nishit Suwal <81785002+NSUWAL123@users.noreply.github.com>
2025-09-26 10:43:25 +0545

    Fix frontend issues (#2836)
    * fix(createProjectService): remove action dispatch
    
    * fix(banner): remove z-index

--- src/frontend/src/components/Landing/Banner.tsx
+++ src/frontend/src/components/Landing/Banner.tsx
@@ -11,7 +11,7 @@ const Banner = () => {
       style={{ backgroundImage: `url(${landingbg})` }}
       className={`fmtm-w-full fmtm-h-[38rem] fmtm-bg-no-repeat fmtm-bg-cover fmtm-relative fmtm-flex fmtm-items-center`}
     >
-      <div className="fmtm-absolute fmtm-left-0 fmtm-top-0 fmtm-w-full fmtm-h-full fmtm-bg-black/40 fmtm-z-10" />
+      <div className="fmtm-absolute fmtm-left-0 fmtm-top-0 fmtm-w-full fmtm-h-full fmtm-bg-black/40" />
       <div className="fmtm-text-white fmtm-z-50 fmtm-relative fmtm-ml-[10%] fmtm-flex fmtm-flex-col fmtm-gap-y-5">
         <h1 className="fmtm-text-[2.5rem] md:fmtm-text-[3.625rem]">FIELD - TASKING MANAGER</h1>
         <h4 className="fmtm-text-[1rem] md:fmtm-text-[1.25rem] ">
