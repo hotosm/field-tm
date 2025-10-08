a06d7a059844e8346bb13023e2e512eafe50fe7a
Nishit Suwal <suwalnishit@gmail.com>
2025-10-08 09:14:42 +0000

    fix(+error): show login modal if logged out user opens private project

--- src/mapper/src/routes/project/[projectId]/+error.svelte
+++ src/mapper/src/routes/project/[projectId]/+error.svelte
@@ -1,5 +1,16 @@
 <script>
 	import { page } from '$app/stores';
+	import { getLoginStore } from '$store/login.svelte.ts';
+
+	const loginStore = getLoginStore();
+
+	$effect(() => {
+		if ($page.status === 403 && !loginStore.getAuthDetails) {
+			loginStore.toggleLoginModal(true);
+		} else {
+			loginStore.toggleLoginModal(false);
+		}
+	});
 </script>
 
 <h1>{$page.error.message}</h1>
