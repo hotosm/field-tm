2dddadc3799c2a51a24a73e382a14d643b4c7dba
spwoodcock <sam.woodcock@protonmail.com>
2025-10-19 20:04:50 +0100

    docs: info about splitting algo placeholder

---
+++ src/backend/packages/area-splitter/area_splitter/algorithms/common/4-algo.sql
@@ -0,0 +1,5 @@
+-- This is a placeholder to insert the algorithms code from one directory higher.
+-- E.g. ../avg_building_voronoi.sql
+
+-- There are many ideas for potential algorithms to implement, but little time do to do!
+-- PRs always welcome 🙏
