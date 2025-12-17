0dc76eae06befc665bc9321828ef15499c9a24ff
Nishit Suwal <suwalnishit@gmail.com>
2025-12-17 08:10:26 +0000

    refactor(organisation): remove custom query hooks, types, services

--- src/frontend/src/types/index.ts
+++ src/frontend/src/types/index.ts
@@ -4,7 +4,6 @@ import type { UseQueryOptions, QueryKey, UseMutationOptions } from '@tanstack/re
 export * from '@/api/auth/types';
 export * from '@/api/central/types';
 export * from '@/api/helper/types';
-export * from '@/api/organisation/types';
 export * from '@/api/project/types';
 export * from '@/api/submission/types';
 export * from '@/api/task/types';
