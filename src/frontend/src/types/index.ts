88aeee95a5cec2c8d0a99ce6e16f0f77be72a8a7
Nishit Suwal <suwalnishit@gmail.com>
2025-12-17 08:20:23 +0000

    refactor(submission): remove custom query hook, types, services

--- src/frontend/src/types/index.ts
+++ src/frontend/src/types/index.ts
@@ -5,7 +5,6 @@ export * from '@/api/auth/types';
 export * from '@/api/central/types';
 export * from '@/api/helper/types';
 export * from '@/api/project/types';
-export * from '@/api/submission/types';
 export * from '@/api/task/types';
 export * from '@/api/user/types';
 
