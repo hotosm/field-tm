4833e2af5c34de32862f3b04a2eaab21fe887cbf
Nishit Suwal <suwalnishit@gmail.com>
2025-12-17 05:18:27 +0000

    refactor(organization): remove

--- src/frontend/src/components/CreateEditOrganization/constants/consentDefaultValues.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod/v4';
-import { consentValidationSchema } from '../validation/ConsentDetailsValidation';
-
-export const consentDefaultValues: z.infer<typeof consentValidationSchema> = {
-  give_consent: '',
-  review_documentation: [],
-  participated_in: [],
-};
