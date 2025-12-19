e7f4ec04459760cc6314e91a8645604d988320cf
Nishit Suwal <suwalnishit@gmail.com>
2025-12-19 09:28:15 +0000

    refactor(models): unused ts types

--- src/frontend/src/models/home/homeModel.ts
+++ src/frontend/src/models/home/homeModel.ts
@@ -1,17 +1,5 @@
 import { project_status, project_visibility } from '@/types/enums';
 
-export interface HomeProjectCardModel {
-  id: number;
-  name: string;
-  priority: number;
-  location_str: string;
-  description: string;
-  total_tasks: number;
-  tasks_mapped: number;
-  tasks_validated: number;
-  tasks_bad: number;
-}
-
 export type projectType = {
   name: string;
   centroid: [number, number];
--- src/frontend/src/store/types/ISubmissions.ts
+++ src/frontend/src/store/types/ISubmissions.ts
@@ -1,12 +1,3 @@
-export type filterType = {
-  task_id: string | null;
-  submitted_by: string | null;
-  review_state: string | null;
-  submitted_date_range: string | null;
-  page: number;
-  results_per_page: number;
-};
-
 export type featureType = {
   type: 'Feature';
   geometry: Partial<{
