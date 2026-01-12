60bc0e74f2cd0077f906e718eb6474260ef72273
Amit Khandekar <64206751+amitdkhan-pg@users.noreply.github.com>
2026-01-12 13:47:01 +0530

    feat(area-splitter): Use Straight Skeleton algorithm to split tasks. (#2999)
    * feat(area-splitter): Use Straight Skeleton algorithm to split tasks.
    
    This replaces the previous Voronoi algorithm.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/backend/packages/area-splitter/area_splitter/algorithms/common/6-alignment.sql
+++ src/backend/packages/area-splitter/area_splitter/algorithms/common/6-alignment.sql
@@ -1,3 +1,10 @@
+-- Build as index on the taskpolygons
+ALTER TABLE taskpolygons ADD PRIMARY KEY (taskid);
+SELECT POPULATE_GEOMETRY_COLUMNS('public.taskpolygons'::regclass);
+CREATE INDEX taskpolygons_idx
+ON taskpolygons
+USING gist (geom);
+
 -- Merge least feature polygons with neighbouring polygons
 DO $$
 DECLARE
